4975681db4869ddbcef505a0306e8c7a1143faf9
koedi <kari.pietikainen@gmail.com>
2020-11-29 11:28:44 +0200

    week 5 ex 5 - LaskinFX11

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Sovelluslogiikka.java
@@ -0,0 +1,15 @@
+package laskin;
+
+public class Sovelluslogiikka {
+ 
+    private int tulos;
+ 
+    public int getTulos() {
+        return tulos;
+    }
+
+    public void setTulos(int arvo) {
+        this.tulos = arvo;
+    }
+    
+}
---
+++ viikko5/LaskinFX11/src/main/java/laskin/Summa.java
@@ -0,0 +1,41 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = 0;
+
+        try {
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+        }
+
+        this.sovellus.setTulos(this.sovellus.getTulos() + arvo);
+
+        int laskunTulos = sovellus.getTulos();
+        
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+        
+        if ( laskunTulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+     
+    }
+
+
+}
