ef965f0240e582993ebb611e4cd941a088dd3000
koedi <kari.pietikainen@gmail.com>
2020-12-05 17:17:52 +0200

    week 6 - ex 3

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
           
-        Matcher m = new And( new HasAtLeast(5, "goals"),
+        Matcher m1 = new And( new HasAtLeast(5, "goals"),
                              new HasAtLeast(5, "assists"),
                              new PlaysIn("PHI")
         );
-        for (Player player : stats.matches(m)) {
+        for (Player player : stats.matches(m1)) {
             System.out.println(player);
         }
-        
+
         System.out.println("---");
 
         Matcher m2 = new And( new Not( new HasAtLeast(1, "goals") ), 
@@ -33,6 +33,29 @@ public class Main {
         for (Player player : stats.matches(m3)) {
             System.out.println(player);
         }
+
+        System.out.println("---");
+
+        Matcher m4 = new Or( new HasAtLeast(40, "goals"),
+                    new HasAtLeast(60, "assists")
+        ); 
+        for (Player player : stats.matches(m4)) {
+            System.out.println(player);
+        }
+
+        System.out.println("---");
+
+        Matcher m5 = new And( new HasAtLeast(50, "points"),
+                              new Or( new PlaysIn("NYR"),
+                                      new PlaysIn("NYI"),
+                                      new PlaysIn("BOS")
+                                    )
+        ); 
+        for (Player player : stats.matches(m5)) {
+            System.out.println(player);
+        }
+
+
 
 
     }
